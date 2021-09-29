bd67b03c92996b2fcb5a41e7c2bf3d9037e9386a
kimffy24 <kimffy24@gmail.com>
2021-09-30 03:05:15 +0800

    支持到 uow:0.1.0.0 & uowgen:0.1.0.1 ; 相当于同步两者版本并支持

--- src/main/java/pro/jiefzz/demo/uowdemo/expose/http/FirstController.java
+++ src/main/java/pro/jiefzz/demo/uowdemo/expose/http/FirstController.java
@@ -6,10 +6,13 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import pro.jiefzz.demo.uowdemo.aggr.demo.BankAccount;
+import pro.jiefzz.demo.uowdemo.service.BankAccountService;
 import pro.jiefzz.demo.uowdemo.service.UserEntranceService;
 
 @RestController
@@ -27,4 +30,21 @@ public class FirstController {
 		return new HashMap<>();
 	}
 	
+	@Autowired
+	private BankAccountService bankAccountService;
+
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	public Map<String, Object> test(@RequestBody AccountCreationForm cForm) {
+		
+		BankAccount createAccount = bankAccountService.createAccount(cForm.userName, cForm.currencyCode);
+		
+		System.out.println("create new account: " + createAccount);
+		return new HashMap<>();
+		
+	}
+	
+	public static class AccountCreationForm {
+		public String userName;
+		public String currencyCode;
+	}
 }
